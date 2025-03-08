1c3ce66d0d81215cf9ca8b081afed6729eda2172
111495zjy <2020011631@student.cup.edu.cn>
2025-03-08 19:26:10 +0000

    Update txt_npy.py

--- txt_npy.py
+++ txt_npy.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-txt_file_path = '/content/EvINR/ECD/dynamic_6dof/events.txt'
-npy_file_path = '/content/EvINR/ECD/dynamic_6dof/events.npy'
+txt_file_path = '/content/EvINR_model_connection/gun_bullet_gnome.txt'
+npy_file_path = '/content/EvINR_model_connection/gun_bullet_gnome.npy'
+
 events = []
 with open(txt_file_path, 'r') as f:
+    #next(f)  # 跳过第一行
     for line in f:
         t, x, y, p = map(float, line.strip().split())
         events.append([t, x, y, p])
+
 events_array = np.array(events)
 np.save(npy_file_path, events_array)
